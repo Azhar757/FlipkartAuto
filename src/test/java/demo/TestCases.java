fa6f60600ff1368f61ce1ad0ae2e78d95032582d
Azhar <azharrehman17@gmail.com>
2024-09-25 00:22:00 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,16 +1,23 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.text.NumberFormat;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -25,6 +32,186 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+
+Wrappers wrappers = new Wrappers();
+
+
+    @Test
+ public void  testCase01() throws InterruptedException{ 
+
+driver.get("https://www.flipkart.com/");
+
+wrappers.SearchMain(driver, "Washing Machine");
+driver.findElement(By.xpath("//div[text()='Popularity']")).click();
+// List<WebElement>rating =driver.findElements(By.xpath("(//span[contains(@id,'productRating_LSTWM')]/div)[24]"));
+wrappers.scrollToEndOfPage(driver);
+Thread.sleep(1000);
+
+int count = 0;
+for(int i=1; i<=23; i++){
+
+String rate=driver.findElement(By.xpath("(//span[contains(@id,'productRating_LSTWM')]/div)["+i+"]")).getText();
+
+//System.out.println(rate);
+count++;
+System.out.println("count = "+count);
+Double rateint= Double.parseDouble(rate);
+if(rateint<=4){
+
+    System.out.println(rate);
+}
+
+
+
+}
+
+
+}
+
+@Test
+public void  testCase02() throws InterruptedException{ 
+  
+
+
+    driver.get("https://www.flipkart.com/");
+    Thread.sleep(3000);
+    try {
+    
+        driver.findElement(By.xpath("//div[@class='JFPqaw']/span"));
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@class='JFPqaw']/span")).click();
+    } catch (Exception e) {
+        System.out.println("Login Alert Box Appared");
+        // TODO: handle exception
+    }
+    wrappers.SearchMain(driver, "iPhone");
+
+    wrappers.scrollToEndOfPage(driver);
+    Thread.sleep(1000);
+int count1= 0;
+
+    for(int i=1; i<=24;i++){
+ 
+try{
+
+
+        WebElement IphonPercentage = driver.findElement(By.xpath("(//div[@class='yKfJKb row'])["+i+"]//div[@class='UkUFwK']"));
+      String Discount =  IphonPercentage.getText();
+// Use regex to extract the numeric part from the string
+String discountNumber = Discount.replaceAll("\\D+", "");  // This will remove all non-digit characters
+
+// Convert the extracted number to an integer
+int discountInt = Integer.parseInt(discountNumber);
+
+
+      if(discountInt>6){
+
+      WebElement  IphoneTitel = driver.findElement(By.xpath("(//div[@class='yKfJKb row'])["+i+"]//div[@class='KzDlHZ']"));
+         String IphonT=IphoneTitel.getText();
+
+        System.out.println(IphonT +" " + " " + discountInt);
+      }
+
+
+    } catch (NoSuchElementException e) {
+count1++;       
+ //System.out.println("NoSuchElementException = "+count1 );
+        // TODO: handle exception
+    }
+
+
+    }
+    
+    System.out.println("Count of Iphone which are not having discount > expected = "+count1 );
+
+
+
+
+
+
+}
+@Test
+public void testCase03()throws InterruptedException{
+
+
+    driver.get("https://www.flipkart.com/");
+    Thread.sleep(3000);
+    try {
+    
+        driver.findElement(By.xpath("//div[@class='JFPqaw']/span"));
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@class='JFPqaw']/span")).click();
+    } catch (Exception e) {
+        System.out.println("Login Alert Box Appared");
+        // TODO: handle exception
+    }
+    wrappers.SearchMain(driver, "Coffee Mug");
+    WebElement check =driver.findElement(By.xpath("//span[text()='Price']"));
+    wrappers.ScrollToElement(driver, check);
+    Thread.sleep(1000);
+
+driver.findElement(By.xpath("(//label[@class='tJjCVx _3DvUAf'])[1]/div[@class='XqNaEv']")).click();
+Thread.sleep(4000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for the element to be visible
+        WebElement star4 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='YcSYyC']")));
+        Thread.sleep(3000);
+wrappers.scrollToEndOfPage(driver);
+                // Get all elements with the class 'Wphh3N'
+        List<WebElement> elements = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+
+        // Create a list to store the numbers after removing the parentheses
+        List<Integer> numbers = new ArrayList<>();
+
+        // Loop through the elements, remove parentheses, and convert text to an integer
+        for (WebElement element : elements) {
+            String text = element.getText();  // Example: "(608)"
+            text = text.replaceAll("[(),]", "");  // Remove parentheses
+            int number = Integer.parseInt(text);  // Convert to integer
+            numbers.add(number);  // Add to the list
+        }
+
+        // Sort the numbers in descending order
+        Collections.sort(numbers, Collections.reverseOrder());
+
+        // Print the sorted numbers
+     //   System.out.println("Sorted numbers in descending order: " + numbers);
+
+   List<Integer> numbers1 = new ArrayList<>();
+        numbers1.add(numbers.get(0));
+        numbers1.add(numbers.get(1));
+        numbers1.add(numbers.get(2));
+        numbers1.add(numbers.get(3));
+        numbers1.add(numbers.get(4));
+
+        // Create a list to store formatted numbers
+        List<String> formattedNumbers = new ArrayList<>();
+
+        // Loop through the top 5 numbers and format them
+        for (int number : numbers1) {
+            // Format the number with commas
+            String formattedNumber = NumberFormat.getInstance().format(number);
+            formattedNumbers.add(formattedNumber);
+        }
+
+for (int i=0; i<=4; i++){
+   WebElement imageElement= driver.findElement(By.xpath("(//span[text()='("+formattedNumbers.get(i)+")']/parent::div/parent::div//a)[1]"));
+    String imageUrl = imageElement.getAttribute("href");
+
+    WebElement ElementTitel= driver.findElement(By.xpath("(//span[text()='("+formattedNumbers.get(i)+")']/parent::div/parent::div//a)[2]"));
+    String Titel = ElementTitel.getText();
+
+    System.out.println(imageUrl);
+    System.out.println(Titel);
+
+}
+       
+
+
+
+
+}//span[text()='Price']
 
      
     /*
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,4 +20,30 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+ public void    SearchMain(WebDriver driver ,String toSearch) throws InterruptedException{
+
+
+WebElement searchfield =driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+searchfield.sendKeys(toSearch);
+WebElement searchButton =driver.findElement(By.xpath("//button[@title='Search for Products, Brands and More']"));
+Thread.sleep(800);
+searchButton.click();
+Thread.sleep(3800);
+
+
+}
+
+public void ScrollToElement (WebDriver driver , WebElement element){
+
+    JavascriptExecutor js = (JavascriptExecutor) driver; 
+    js.executeScript("arguments[0].scrollIntoView(true);", element);
+
+
+}
+public void scrollToEndOfPage(WebDriver driver) {
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+}
+
 }
